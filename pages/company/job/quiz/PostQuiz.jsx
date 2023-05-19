ac28b1801e41ec81ab9d0c5bdbb4c8ba7005c9a4
HammadJ <hammadjavaid2@gmail.com>
2023-05-20 00:45:06 +0500

    postquiz

--- pages/company/job/quiz/PostQuiz.jsx
+++ pages/company/job/quiz/PostQuiz.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import axios from 'axios';
 import Router, { useRouter } from 'next/router';
+import AddIcon from '@mui/icons-material/AddCircleOutlined';
+import RemoveIcon from '@mui/icons-material/RemoveCircleOutlined';
+
 
 const PostQuiz = () => {
 
@@ -68,7 +71,7 @@ const PostQuiz = () => {
         event.preventDefault();
 
         console.log(jobId);
-        const res = await axios.post(`/api/company/job/postQuiz`, { _id: jobId, quiz:questions }, { headers: { 'Content-Type': 'application/json' } });
+        const res = await axios.post(`/api/company/job/postQuiz`, { _id: jobId, quiz: questions }, { headers: { 'Content-Type': 'application/json' } });
         if (res.status === 200)
             Router.push('/company/job');
         console.log('Questions:', questions);
@@ -78,47 +81,54 @@ const PostQuiz = () => {
 
     return (
         <>
-            <div>down</div>
-            <div>down</div>
-            <div>down</div>
-            <div>down</div>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} className="quiz-form">
                 {questions.map((question, questionIndex) => (
-                    <div key={questionIndex}>
-                        <div>Question {questionIndex + 1}</div>
+                    <div key={questionIndex} className="question-container">
+                        <div className="question-heading">Question {questionIndex + 1}</div>
                         <input
                             label="Question"
                             name={`question${questionIndex}`}
                             value={question.question}
                             onChange={(event) => handleChangeQuestion(event, questionIndex)}
+                            className="question-input"
                         />
 
-                        <div>
+                        <div className="question-type">
                             <label>Question Type: </label>
-                            <select value={question.type} onChange={(event) => handleChangeType(event, questionIndex)}>
+                            <select
+                                value={question.type}
+                                onChange={(event) => handleChangeType(event, questionIndex)}
+                                className="type-select"
+                            >
                                 <option value="" disabled>Select Type</option>
                                 <option value="single">Single Correct</option>
-                                <option value="multiple" >Multiple Correct</option>
+                                <option value="multiple">Multiple Correct</option>
                             </select>
                         </div>
 
                         {question.options.map((option, optionIndex) => (
-                            <div key={optionIndex}>
-                                <div>Option {optionIndex + 1}</div>
+                            <div key={optionIndex} className="option-container">
+                                <div className="option-heading">Option {optionIndex + 1}</div>
 
-                                {question.type === 'single' ? (
+                                {question.type === "single" ? (
                                     <input
                                         type="radio"
                                         name={`correctOption${questionIndex}`}
                                         checked={option.isCorrect}
-                                        onChange={(event) => handleChangeCorrectOption(event, questionIndex, optionIndex)}
+                                        onChange={(event) =>
+                                            handleChangeCorrectOption(event, questionIndex, optionIndex)
+                                        }
+                                        className="radio-input"
                                     />
                                 ) : (
                                     <input
                                         type="checkbox"
                                         name={`correctOption${questionIndex}`}
                                         checked={option.isCorrect}
-                                        onChange={(event) => handleChangeCorrectOption(event, questionIndex, optionIndex)}
+                                        onChange={(event) =>
+                                            handleChangeCorrectOption(event, questionIndex, optionIndex)
+                                        }
+                                        className="checkbox-input"
                                     />
                                 )}
 
@@ -126,31 +136,53 @@ const PostQuiz = () => {
                                     label="Option"
                                     name={`option${optionIndex}`}
                                     value={option.option}
-                                    onChange={(event) => handleChangeOption(event, questionIndex, optionIndex)}
+                                    onChange={(event) =>
+                                        handleChangeOption(event, questionIndex, optionIndex)
+                                    }
+                                    className="option-input"
                                 />
-
-                                <button type="button" onClick={() => removeOptionFields(questionIndex, optionIndex)}>
+                                <RemoveIcon fontSize="small" color="error" className="remove-option-button" onClick={() => removeOptionFields(questionIndex, optionIndex)} />
+                                {/* <button
+                                    type="button"
+                                    onClick={() => removeOptionFields(questionIndex, optionIndex)}
+                                    className="remove-option-button"
+                                >
                                     Remove Option
-                                </button>
+                                </button> */}
                             </div>
                         ))}
-                        <button type="button" onClick={() => addOptionFields(questionIndex)}>
+                        <AddIcon fontSize='small' color='secondary' className="add-option-button"
+                            onClick={() => addOptionFields(questionIndex)} />
+                        {/* <button
+                            type="button"
+                            onClick={() => addOptionFields(questionIndex)}
+                            className="add-option-button"
+                        >
                             Add Option
-                        </button>
+                        </button> */}
                         {questions.length !== 1 && (
-                            <button type="button" onClick={() => removeQuestionFields(questionIndex)}>
+                            <button
+                                type="button"
+                                onClick={() => removeQuestionFields(questionIndex)}
+                                className="remove-question-button"
+                            >
                                 Remove Question
                             </button>
                         )}
                     </div>
                 ))}
-                <button type="button" onClick={() => addQuestionFields()}>
+                <button
+                    type="button"
+                    onClick={() => addQuestionFields()}
+                    className="add-question-button"
+                >
                     Add Question
                 </button>
-                <button type="submit">Submit Quiz</button>
+                <button type="submit" className="submit-button">Submit Quiz</button>
             </form>
         </>
     );
+
 }
 
 
