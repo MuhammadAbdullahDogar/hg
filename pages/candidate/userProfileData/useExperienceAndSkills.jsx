eda35e2a789b87cc61e82cc8456ba1dbcf3a33d3
HammadJ <hammadjavaid2@gmail.com>
2023-06-06 01:18:02 +0500

    experienceandskills

--- pages/candidate/userProfileData/useExperienceAndSkills.jsx
+++ pages/candidate/userProfileData/useExperienceAndSkills.jsx
@@ -11,23 +11,23 @@ const useExperienceAndSkills = ({ experiences }) => {
                             <Grid item container spacing={4}>
                                 <Grid item xs={12}></Grid>
                                 <Grid item xs={.5}></Grid>
-                                <Grid item xs={11}><Typography variant="displayh1">{experience?.jobTitle} Advance Web Developer</Typography></Grid>
+                                <Grid item xs={11}><Typography variant="displayh1">{experience?.jobTitle}</Typography></Grid>
                                 <Grid item xs={.5}></Grid>
 
                                 <Grid item xs={.5}></Grid>
-                                <Grid item xs={5}><Typography variant="displayh4">Company Name</Typography><br /><Typography variant="displayh5">{experience?.cName} Web Technologies</Typography></Grid>
+                                <Grid item xs={5}><Typography variant="displayh4">Company Name</Typography><br /><Typography variant="displayh5">{experience?.cName}</Typography></Grid>
                                 <Grid item xs={1}></Grid>
-                                <Grid item xs={5.5}><Typography variant="displayh4">Company Domin</Typography><br /><Typography variant="displayh5">{experience?.cDomain} Software Services</Typography></Grid>
+                                <Grid item xs={5.5}><Typography variant="displayh4">Company Domin</Typography><br /><Typography variant="displayh5">{experience?.cDomain}</Typography></Grid>
 
                                 <Grid item xs={.5}></Grid>
-                                <Grid item xs={5}><Typography variant="displayh4">Starting Date</Typography><br /><Typography variant="displayh5">{experience?.startingDate} July 10,2018</Typography></Grid>
+                                <Grid item xs={5}><Typography variant="displayh4">Starting Date</Typography><br /><Typography variant="displayh5">{experience?.startingDate} </Typography></Grid>
                                 <Grid item xs={1}></Grid>
-                                <Grid item xs={5.5}><Typography variant="displayh4">Ending Date</Typography><br /><Typography variant="displayh5">{experience?.endingDate} March 02,2019</Typography></Grid>
+                                <Grid item xs={5.5}><Typography variant="displayh4">Ending Date</Typography><br /><Typography variant="displayh5">{experience?.endingDate} </Typography></Grid>
 
                                 <Grid item xs={.5}></Grid>
                                 <Grid item xs={11.5}><Typography variant="displayh4">Key Responsibilities:</Typography></Grid>
                                 <Grid item xs={.5}></Grid>
-                                <Grid item xs={11}><Typography variant="displayh5">{experience?.responsibities} Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. </Typography></Grid>
+                                <Grid item xs={11}><Typography variant="displayh5">{experience?.responsibities}  </Typography></Grid>
                                 <Grid item xs={.5}></Grid>
                             </Grid>
                         </React.Fragment>
